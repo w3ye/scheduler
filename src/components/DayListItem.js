7e16a36fc1030773d5276b09948c820b37f43265
William Ye <w3ye@pm.me>
2021-08-31 22:31:26 -0400

    add DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "components/DayListItem.scss";
+const classNames = require("classnames");
+
+function formatSpots(spot) {
+  if (spot === 0) return "no spots remaining";
+  else if (spot === 1) return "1 spot remaining";
+  else return `${spot} spots remaining`;
+}
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+
+  console.log(dayClass);
+  return (
+    <li onClick={() => setDay(name)} className={dayClass}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
+    </li>
+  );
+}
