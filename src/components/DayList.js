da5bd7fa1dcb5b37e84cadd3c1d97035b962c98f
William Ye <w3ye@pm.me>
2021-08-31 23:30:57 -0400

    add DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+  const parsedDays = days.map((i) => {
+    return <DayListItem {...i} key={i.id} setDay={setDay} selected={i.name === day} />;
+  });
+
+  return <ul>{parsedDays}</ul>;
+}
