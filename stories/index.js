5f05024a0de00879f3b8784f45b331663250800c
William Ye <w3ye@pm.me>
2021-09-01 22:24:30 -0400

    add Confirm to storybook

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confrim from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,5 +146,12 @@ storiesOf("Appointment", module)
       interviewer="Sylvia Palmer"
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confrim
+      message={"Delete the appointment ?"}
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />
   ));
