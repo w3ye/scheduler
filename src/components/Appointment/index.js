0aca9f066a04d52d2f7aadcace505cca89d99ed3
William Ye <w3ye@pm.me>
2021-09-02 00:32:53 -0400

    implement Appointment components to index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,15 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
+import Empty from "components/Appointment/Empty";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
 
-export default function index() {
-  return <article className="appointment"></article>;
+export default function index(props) {
+  console.log(props.interview);
+  return (
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
+  );
 }
