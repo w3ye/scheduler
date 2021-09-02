c57a12264e6619442199d417f9a3eabf8be4fcbb
William Ye <w3ye@pm.me>
2021-09-02 15:36:11 -0400

    use reset to destructure the interviewer obj

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,8 @@ import InterviewerListItem from 'components/InterviewerListItem'
 export default function InterviewerList(props) {
   const { interviewers, value, onChange } = props;
   const parsedInterviewers = interviewers.map((i) => {
-    return <InterviewerListItem key={i.id} {...i} selected={i.id === value} setInterviewer={() => onChange(i.id)}/>
+    const {id, ...rest} = i;
+    return <InterviewerListItem key={id} {...rest} selected={id === value} setInterviewer={() => onChange(i.id)}/>
   });
   return (
     <section className="interviewers">
