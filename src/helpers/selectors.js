b2c90d7fc04c5301fb89ce13eec6d2b55973e570
William Ye <w3ye@pm.me>
2021-09-03 18:44:22 -0400

    implement getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,16 @@ export function getAppointmentsForDay(state, day) {
   });
   return resultArr;
 }
+
+export function getInterview(state, interview) {
+  const ret = {};
+  for(let key in state.interviewers) {
+    if (interview === null) return null;
+    // when the interviwer in interview matches with interviewer
+    if (state.interviewers[key].id === interview.interviewer) {
+      ret['student'] = interview.student;
+      ret['interviewer'] = state.interviewers[key];
+    } 
+  }
+  return ret;
+}
