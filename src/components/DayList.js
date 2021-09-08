39ea130672106acc1bd137f9210dc35d6fc48020
William Ye <w3ye@pm.me>
2021-09-08 18:45:06 -0400

    add cypress

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,15 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const { days, day, setDay } = props;
   const parsedDays = days.map((i) => {
-    return <DayListItem {...i} key={i.id} setDay={setDay} selected={i.name === day} />;
+    return (
+      <DayListItem
+        {...i}
+        key={i.id}
+        setDay={setDay}
+        selected={i.name === day}
+        data-testid="day"
+      />
+    );
   });
 
   return <ul>{parsedDays}</ul>;
